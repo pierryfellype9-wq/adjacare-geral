29d7ac48fe6205939ffaf7beaabf5a6715fc8b5f
Pierry Fernandes <pierryfellype9@gmail.com>
2026-03-13 17:28:39 -0300

    Update enviar-email.js

--- api/enviar-email.js
+++ api/enviar-email.js
@@ -4,26 +4,24 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default async function handler(req, res){
 
-  const { email, titulo } = req.body
-
   try{
 
+    const { assunto, mensagem } = req.body
+
     await resend.emails.send({
-      from: "Sistema ADJACARE <sistema@adjacare.org>",
-      to: email,
-      subject: "Solicitação recebida",
-      html: `
-        <h2>Recebemos sua solicitação</h2>
-        <p><b>Título:</b> ${titulo}</p>
-        <p>Nossa equipe já foi notificada.</p>
-      `
+
+      from: "Sistema ADJACARÉ <midia@adjacare.org>",
+      to: ["midia@adjacare.org"],
+      subject: assunto,
+      html: mensagem
+
     })
 
-    res.status(200).json({ok:true})
+    res.status(200).json({ ok:true })
 
   }catch(err){
 
-    res.status(500).json({error:err.message})
+    res.status(500).json({ error:err.message })
 
   }
 

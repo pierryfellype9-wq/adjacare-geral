3a0f31a7e236644d27ff0ec1b6e08c0910cf97ba
Pierry Fernandes <pierryfellype9@gmail.com>
2026-03-14 01:52:41 -0300

    Update kommoWebhook.js

--- api/kommoWebhook.js
+++ api/kommoWebhook.js
@@ -1,11 +1,41 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
+
 export default async function handler(req, res) {
 
-  console.log("Webhook recebido")
+  console.log("Webhook recebido:", req.body)
 
-  console.log("METHOD:", req.method)
+  if (req.method !== "POST") {
+    return res.status(200).json({ ok: true })
+  }
 
-  console.log("BODY:", req.body)
+  const body = req.body
 
-  return res.status(200).json({ ok: true })
+  if (!body["message[add][0][text]"]) {
+    return res.status(200).json({ ok: true })
+  }
+
+  const mensagem = body["message[add][0][text]"]
+
+  await supabase
+    .from("pedidos")
+    .insert([
+      {
+        titulo: "Pedido via WhatsApp",
+        descricao: mensagem,
+        prioridade: "Normal",
+        destino: "Mídia",
+        ministerio: "WhatsApp",
+        criado_por: "WhatsApp",
+        status: "Pendente",
+        data: new Date().toISOString()
+      }
+    ])
+
+  res.status(200).json({ ok: true })
 
 }

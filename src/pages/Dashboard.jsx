17d5729fc96d90cf15e1dcc9cac81debdb13e476
Pierry Fernandes <pierryfellype9@gmail.com>
2026-03-16 16:13:59 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -99,7 +99,9 @@ export default function Dashboard({ user }){
 
               </strong>
 
-              <p>{a.mensagem}</p>
+              <p style={{whiteSpace:"pre-line"}}>
+                {a.mensagem}
+              </p>
 
             </div>
 

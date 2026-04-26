77bc0cbbad3cf5827b649688776608ef7d8f241f
Pierry Fernandes <pierryfellype9@gmail.com>
2026-04-26 00:22:23 -0300

    Update EBDChamada.jsx

--- src/pages/EBDChamada.jsx
+++ src/pages/EBDChamada.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { supabase } from "../supabase"
+import { supabase } from "../lib/supabase"
 
 export default function EBDChamada({ user }) {
   const usuario = user || JSON.parse(localStorage.getItem("user") || "{}")

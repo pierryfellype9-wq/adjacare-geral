8a4f328ac2dca5987bc742992ec8025afd1aa513
Pierry Fernandes <pierryfellype9@gmail.com>
2026-04-26 00:22:41 -0300

    Update EBDRelatorios.jsx

--- src/pages/EBDRelatorios.jsx
+++ src/pages/EBDRelatorios.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { supabase } from "../supabase"
+import { supabase } from "../lib/supabase"
 
 export default function EBDRelatorios() {
   const [dados, setDados] = useState([])

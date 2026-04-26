445053d3bfcdebf163d0c5ec61c77a433b7fa900
Pierry Fernandes <pierryfellype9@gmail.com>
2026-04-26 00:22:11 -0300

    Update EBDAlunos.jsx

--- src/pages/EBDAlunos.jsx
+++ src/pages/EBDAlunos.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { supabase } from "../supabase"
+import { supabase } from "../lib/supabase"
 
 export default function EBDAlunos() {
   const [turmas, setTurmas] = useState([])

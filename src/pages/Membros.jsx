b86ec75f7457aade54e6b04f0acadaef0d74b56f
Pierry Fernandes <pierryfellype9@gmail.com>
2026-05-31 19:38:04 -0300

    Update Membros.jsx

--- src/pages/Membros.jsx
+++ src/pages/Membros.jsx
@@ -185,6 +185,7 @@ export default function Membros({ user }) {
             <option value="Solteiro">Solteiro</option>
             <option value="Casado">Casado</option>
             <option value="Divorciado">Divorciado</option>
+            <option value="Viuvo">Divorciado</option>
           </select>
 
           <select

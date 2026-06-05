0619af237c804370406d82ead96975961bbff6ee
Pierry Fernandes <pierryfellype9@gmail.com>
2026-06-05 00:18:13 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 )
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+  })
+}
